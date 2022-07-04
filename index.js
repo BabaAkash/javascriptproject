2a66452ae4cdaf7a53a30086f3f8455c45592c27
AKashgitbash <ashakyawwe06@gmail.com>
2022-07-04 21:37:23 +0530

    changes in indexhtml

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
 function mysubmit(){
     var  body = { 
-      
         fullname : document.getElementById("fullname").value,
         mobile_number : document.getElementById("mobile_number").value,
         passyear : document.getElementById("passyear").value,
